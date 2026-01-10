55d97745b75523fb15a459cf4a2713ad9f98a67e
harshith <harshit>
2026-01-10 21:48:25 +0530

    Portfolio

---
+++ about.js
@@ -0,0 +1,18 @@
+
+// let det = document.getElementById('det')
+
+// let about =()=>{
+//     det.innerHTML=`<h1>Hello....</h1>
+//     <p><span>This is Harshith G </span> from <strong>Bengaluru</strong> an aspiring IT professional with hands-on knowledge of Python, SQL, and web technologies, seeking a role to apply
+// analytical thinking and develop scalable solutions while continuously learning modern tools and frameworks.
+//     </p>` 
+//}
+
+// let education = () =>{
+//     det.innerHTML=`<h1 class="clg"> Masters Of Computer Applications (MCA).</h1>
+//     <h2>Bangalore Institute of Technology.</h2>
+//     <h3>Passing Year : 2025 <br> Cgpa : 8.2</h3>
+//     <h1 class="clg">Bachelor of Science (B'sc).</h1>
+//     <h2>Vijaya College.</h2>
+//     <h3>Passing Year : 2023 <br> Cgpa : 7.6</h3>`
+// }
---
+++ skills.js
@@ -0,0 +1,49 @@
+let card = document.getElementsByClassName('skill-card')
+let html = ()=>{
+    card[0].innerHTML=`<h1>HTML/CSS</h1>
+        <p>Proficient in building
+responsive, dynamic, and well-structured
+web pages with HTML and CSS..</p>`
+}
+let html1 = ()=>{
+    card[0].innerHTML=`<img src="./images/html_css.png">
+                    <h3>HTML/CSS</h3>
+                    `
+}
+
+let js = ()=>{
+    card[1].innerHTML=`<h1>Java Script</h1>
+                        <p>Skilled in writing interactive
+and dynamic web functionality using
+JavaScript (ES6+); experienced in DOM
+manipulation and event handling..</p>`
+}
+
+let js1 = ()=>{
+    card[1].innerHTML=`<img src="./images/js.jpg">
+                    <h3>JavaScript</h3>`
+}
+
+let python = ()=>{
+    card[2].innerHTML=`<h1>Python</h1>
+                        <p> Familiar with Python and its core
+concepts including OOP.</p>`
+}
+
+let python1 = ()=>{
+    card[2].innerHTML=`<img src="./images/python.png">
+                    <h3>Python</h3>`
+}
+
+
+let sql = ()=>{
+    card[3].innerHTML=`<h1>Oracel SQL</h1>
+    <p>Experienced with SQL queries
+including CRUD operations, joins, and
+basic database management.</p>`
+}
+
+let sql1 = ()=>{
+    card[3].innerHTML=`<img src="./images/sql.png">
+                    <h3>SQL</h3>`
+}
